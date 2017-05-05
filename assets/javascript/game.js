6efea2c90cf0a3eb1e26941dba186e7a7c79cf3e
Marissa Pels <marissapels@gmail.com>
2017-05-05 18:53:36 -0500

    final upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,78 @@
-
-
+//document ready function that assigns initial variables
 $(document).ready(function(){
 	var wins=0;
 	var losses=0;
+	var userScore=0;
 
-	
+	//chooses a random number that the user must try to add up to
+	var randomNumber=Math.floor(Math.random()*(120-19+1))+19;
+
+	//updates HTML with assigned variables
+	$("#wins").html(wins);
+	$("#losses").html(losses);
+	$("#userScore").html(userScore);
+	$("#randomNumber").html(randomNumber);
+
+	//assigns a random value betwen 1 and 12 for each button variable
+	var button1= Math.floor(Math.random()*12)+1;
+	var button2= Math.floor(Math.random()*12)+1;
+	var button3= Math.floor(Math.random()*12)+1;
+	var button4= Math.floor(Math.random()*12)+1;
+
+	//assigns a random value betwen 1 and 12 for each button variable
+	$("#button1").attr("value", button1);
+	$("#button2").attr("value", button2);
+	$("#button3").attr("value", button3);
+	$("#button4").attr("value", button4);
+
 
 	function restartGame(){
-		var userScore=0;
-		var randomNumber=Math.floor(Math.random()*(120-19+1))+19;
+		userScore=0;
+		$("#userScore").html(userScore);
+		randomNumber=Math.floor(Math.random()*(120-19+1))+19;
+		$("#randomNumber").html(randomNumber);
+		
+		//assigns a random value betwen 1 and 12 for each button variable
+		button1= Math.floor(Math.random()*12)+1;
+		button2= Math.floor(Math.random()*12)+1;
+		button3= Math.floor(Math.random()*12)+1;
+		button4= Math.floor(Math.random()*12)+1;
 
-		if (userScore<randomNumber){
-			$("#randomNumber").html(randomNumber);
-			$("#wins").html(wins);
-			$("#losses").html(losses);
-			$("#userScore").html(userScore);
+		//assigns a random value betwen 1 and 12 for each button variable
+		$("#button1").attr("value", button1);
+		$("#button2").attr("value", button2);
+		$("#button3").attr("value", button3);
+		$("#button4").attr("value", button4);
+	}
 
-			var button1= Math.floor(Math.random()*12)+1;
-			var button2= Math.floor(Math.random()*12)+1;
-			var button3= Math.floor(Math.random()*12)+1;
-			var button4= Math.floor(Math.random()*12)+1;
+	$(".button").on("click", function(){
 
-			$("#button1").val(button1);
-			$("#button2").val(button2);
-			$("#button3").val(button3);
-			$("#button4").val(button4);
+		//assigns variable to value of clicked button
+		var clickedButton= $(this).attr("value");
+		clickedButton=parseInt(clickedButton);
 
-			console.log(button1);
-			console.log(button2);
-			console.log(button3);
-			console.log(button4);
+		//adds clicked button value to user score
+		userScore += clickedButton;
 
-			$(".button").on("click", function(){
-				var clickedButton= this.value;
-				userScore= parseInt(userScore)+parseInt(clickedButton);
-				console.log(userScore);
-				$("#userScore").html(userScore);
-			})
+		//displays user score on HTML
+		$("#userScore").html(userScore);
 
-		}
-
+		//if user score matches the random number, the game is won and restarts
 		if (userScore===randomNumber){
 			wins++;
+			$("#wins").html(wins);
 			restartGame();
 			return;
 		}
 
+		//if user score is greater than the random number, the game is lost and restarts
 		if (userScore>randomNumber){
 			losses++;
 			$("#losses").html(losses);
 			restartGame();
 			return;
-		}
-
-		$("#randomNumber").html(randomNumber);
-		$("#wins").html(wins);
-		$("#losses").html(losses);
-		$("#userScore").html(userScore);
-
-	}
-
-	restartGame();
-
+		}	
+	})
 
 })
 
